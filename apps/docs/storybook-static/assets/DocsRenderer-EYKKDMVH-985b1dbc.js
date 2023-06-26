65009de6ad1a2f849860a9c21df0b34663e27a3e
Daniel Fiala <danfiala23@gmail.com>
2023-06-26 21:47:27 +0200

    Delete apps/docs/storybook-static directory

--- apps/docs/storybook-static/assets/DocsRenderer-EYKKDMVH-985b1dbc.js
+++
@@ -1,2 +0,0 @@
-import{D as d,d as n}from"./chunk-PCJTTTQV-869be234.js";import"./iframe-9894f566.js";import"../sb-preview/runtime.js";import"./index-ebeaab24.js";import"./index-287110cf.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{d as DocsRenderer,n as defaultComponents};
-//# sourceMappingURL=DocsRenderer-EYKKDMVH-985b1dbc.js.map
