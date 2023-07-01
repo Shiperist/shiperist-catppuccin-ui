9499d8e735d6a2f880bb73a6e7e2b02d4f423051
Daniel Fiala <danfiala23@gmail.com>
2023-07-01 22:56:22 +0200

    enhancement(textinput): added text input back

---
+++ packages/components/text-input/src/index.ts
@@ -0,0 +1,2 @@
+export { default as TextInput } from "./textinput";
+export type { TextInputProps } from "./textinput";
---
+++ packages/components/text-input/src/textinput.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+export interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  leadingElement?: React.ElementType | string;
+  trailingElement?: React.ElementType | string;
+  loading?: boolean;
+  disabled?: boolean;
+  error?: boolean;
+  caption?: string;
+}
+
+const TextInput: React.FC<TextInputProps> = ({
+  leadingElement: LeadingElement,
+  trailingElement: TrailingElement,
+  loading = false,
+  disabled = false,
+  error = false,
+  caption,
+  className = "",
+  ...other
+}) => {
+  const baseClass = `flex flex-col ${className}`;
+  const inputClass = `bg-transparent border-0 outline-none flex-grow placeholder-ctp-subtext2 text-ctp-text mx-1 ${
+    disabled ? "cursor-not-allowed" : ""
+  }`;
+  const containerClass = `flex w-full h-full flex-row px-4 bg-transparent border ring-0 border-1 border-ctp-overlay1 rounded-lg h-12 py-2 ${
+    error ? "border-ctp-red hover:border-ctp-red" : `${!disabled ? "hover:border-ctp-subtext1" : ""}`
+  } ${disabled ? "opacity-50 cursor-not-allowed" : ""}`;
+  const iconClass = "text-ctp-overlay1";
+  const captionClass = "pt-2 text-sm text-ctp-overlay2";
+
+  const currentTrailingElement = loading ? (
+    <svg
+      className="animate-spin"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+    </svg>
+  ) : (
+    TrailingElement && (typeof TrailingElement === "string" ? TrailingElement : <TrailingElement className="" />)
+  );
+
+  const currentLeadingElement =
+    typeof LeadingElement === "string" ? LeadingElement : LeadingElement && <LeadingElement className="" />;
+
+  return (
+    <div className={baseClass}>
+      <div className={containerClass}>
+        {currentLeadingElement && <div className={iconClass}>{currentLeadingElement}</div>}
+        <input className={inputClass} disabled={disabled} {...other} />
+        {currentTrailingElement && <div className={iconClass}>{currentTrailingElement}</div>}
+      </div>
+      {caption && <p className={captionClass}>{caption}</p>}
+    </div>
+  );
+};
+
+export default TextInput;
+
+TextInput.displayName = "TextInput";
